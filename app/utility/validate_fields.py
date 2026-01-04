03ed481d06c8953763d2e74667f8b0f3645b9e60
Illia <lpodildaviduk@gmail.com>
2026-01-04 02:51:34 +0100

    .

--- app/utility/validate_fields.py
+++ app/utility/validate_fields.py
@@ -1,14 +1,9 @@
 import emoji
-import re
 
 
 def validate_email_no_emoji(email: str) -> str:
     if emoji.emoji_count(email) > 0:
         raise ValueError("Email cannot contain emojis")
-    if re.search(r"[^a-zA-Z0-9@._\-]", email):
-        raise ValueError(
-            "Email can only contain letters, numbers, @ . _ -"
-        )
     return email
 
 
@@ -21,8 +16,6 @@ def validate_phone_no_emoji(phone: str) -> str:
 def validate_password_allow_unicode_but_no_emoji(pwd: str) -> str:
     if emoji.emoji_count(pwd) > 0:
         raise ValueError("Password cannot contain emojis")
-    if len(pwd) < 8:
-        raise ValueError("Password must be at least 8 characters")
     return pwd
 
 
